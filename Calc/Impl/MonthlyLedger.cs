1d5759a00fe9d8531044308555203621752611f2
Prodromos <Prodromos@DESKTOP-D8B6GTT>
2022-03-17 15:03:49 +0200

    MonthlyLedger Class

---
+++ Calc/Impl/MonthlyLedger.cs
@@ -0,0 +1,23 @@
+﻿using PetShopLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopLib.Impl
+{
+    internal class MonthlyLedger : IMonthlyLedger
+    {
+        public int Year { get; set; }
+        public short Month { get; set; }
+        public decimal Income { get; set; }
+        public decimal Expenses { get; set; }
+        public decimal Total { get; set; }
+
+        public MonthlyLedger()
+        {
+
+        }
+    }
+}
