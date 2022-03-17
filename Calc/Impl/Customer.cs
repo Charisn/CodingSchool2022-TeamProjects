7cbbe3ca0f158ba041c7f2e92c4c3686070467be
Evangelos Mandalis <evanmand@ea.gr>
2022-03-17 14:57:04 +0200

    Customer & Pet classes

---
+++ Calc/Impl/Customer.cs
@@ -0,0 +1,18 @@
+﻿using PetShopLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopLib.Impl
+{
+    internal class Customer : ICustomer
+    {
+        public int Phone { get ; set; }
+        public int TIN { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public Guid ID { get; set; }
+    }
+}
