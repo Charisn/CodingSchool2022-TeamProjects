ca2c940839bbd5689c3682fd92b5762f83db119d
Charalampos Nikolaidis <ch.nikolaidis@hotmail.com>
2022-03-18 17:20:22 +0200

    Pet Report calc cs fully implemented

---
+++ Calc/Impl/PetReport.cs
@@ -0,0 +1,23 @@
+﻿using PetShopLib.Enums;
+using PetShopLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopLib.Impl
+{
+    internal class PetReport : IPetReport
+    {
+        public int Year { get; set; }
+        public short Month { get; set; }
+        public int TotalSold { get; set; }
+        public AnimalTypeEnum AnimalType { get; set; }
+
+        public PetReport()
+        {
+
+        }
+    }
+}
