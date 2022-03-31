fdc40726dd19894e3c050e9c358be0fdf2389d87
Charalampos Nikolaidis <ch.nikolaidis@hotmail.com>
2022-03-31 20:40:20 +0300

    Deleted Migration

--- Session_21/CarService.EF/Configuration/TransactionConfiguration.cs
+++ Session_21/CarService.EF/Configuration/TransactionConfiguration.cs
@@ -14,7 +14,7 @@ internal class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
     public void Configure(EntityTypeBuilder<Transaction> builder)
     {
         builder.HasKey(transaction => transaction.Id);
-
+        
         //builder.HasMany(transaction => transaction.TransactionLines).WithOne().HasForeignKey(transactionLine => transactionLine.Id);
         //builder.HasOne(transaction => transaction.Manager).WithMany().HasForeignKey(transaction => transaction.ManagerID);
         //builder.HasOne(transaction => transaction.Customer).WithMany().HasForeignKey(transaction => transaction.CustomerID);
