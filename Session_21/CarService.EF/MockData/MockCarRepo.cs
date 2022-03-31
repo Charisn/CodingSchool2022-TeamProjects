4e9390c4d9891d456b8c85ce761257d3ed498761
Charalampos Nikolaidis <ch.nikolaidis@hotmail.com>
2022-03-31 21:32:14 +0300

    Added Mock for Cars and Customers

---
+++ Session_21/CarService.EF/MockData/MockCarRepo.cs
@@ -0,0 +1,49 @@
+﻿using CarService.EF.Repositories;
+using CarService.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarService.EF.MockData;
+
+public class MockCarRepo : IEntityRepo<Car>
+{
+    private List<Car> _car = new List<Car>() { new Car(), new Car() };
+    public Task Create(Car entity)
+    {
+        _car.Add(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task Delete(Guid id)
+    {
+        var foundCar = _car.SingleOrDefault(x => x.Id.Equals(id));
+        if (foundCar is null)
+            return Task.CompletedTask;
+        _car.Remove(foundCar);
+        return Task.CompletedTask;
+    }
+
+    public List<Car> GetAll()
+    {
+        return _car;
+    }
+
+    public Car? GetById(Guid id)
+    {
+        return _car.SingleOrDefault(x => x.Id.Equals(id));
+    }
+
+    public Task Update(Guid id, Car entity)
+    {
+        var foundCar = _car.SingleOrDefault(x => x.Id.Equals(id));
+        if (foundCar is null)
+            return Task.CompletedTask;
+        foundCar.Brand = entity.Brand;
+        foundCar.Model = entity.Model;
+        foundCar.RegistrationNumber = entity.RegistrationNumber;
+        return Task.CompletedTask;
+    }
+}
---
+++ Session_21/CarService.EF/MockData/MockCustomerRepo.cs
@@ -0,0 +1,51 @@
+﻿using CarService.EF.Repositories;
+using CarService.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarService.EF.MockData;
+
+public class MockCustomerRepo : IEntityRepo<Customer>
+{
+    private List<Customer> _customer = new List<Customer>() { new Customer(), new Customer() };
+
+    public Task Create(Customer entity)
+    {
+        _customer.Add(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task Delete(Guid id)
+    {
+        var foundCustomer = _customer.SingleOrDefault(x => x.Id.Equals(id));
+        if (foundCustomer is null)
+            return Task.CompletedTask;
+        _customer.Remove(foundCustomer);
+        return Task.CompletedTask;
+    }
+
+    public List<Customer> GetAll()
+    {
+        return _customer;
+    }
+
+    public Customer? GetById(Guid id)
+    {
+        return _customer.SingleOrDefault(x => x.Id.Equals(id));
+    }
+
+    public Task Update(Guid id, Customer entity)
+    {
+        var foundCustomer = _customer.SingleOrDefault(x => x.Id.Equals(id));
+        if (foundCustomer is null)
+            return Task.CompletedTask;
+        foundCustomer.Surname = entity.Surname;
+        foundCustomer.Phone = entity.Phone;
+        foundCustomer.Name = entity.Name;
+        foundCustomer.TIN = entity.TIN;
+        return Task.CompletedTask;
+    }
+}
