f71c654520a61f5782f5ca0b3bcb4cbfe037f0f3
Giannis Eskioglou <johneski65@gmail.com>
2022-04-01 12:00:15 +0300

    customer page update

---
+++ Session_21/CarService.View/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarService.View.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
