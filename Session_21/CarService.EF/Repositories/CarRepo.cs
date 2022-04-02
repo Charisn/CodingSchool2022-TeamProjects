e9d8ff2e9cb244742790ce16408d1db609c8485b
Charalampos Nikolaidis <ch.nikolaidis@hotmail.com>
2022-04-02 12:28:16 +0300

    Car is fully functional

--- Session_21/CarService.EF/Repositories/CarRepo.cs
+++ Session_21/CarService.EF/Repositories/CarRepo.cs
@@ -10,12 +10,16 @@ namespace CarService.EF.Repositories;
 
 public class CarRepo : IEntityRepo<Car>
 {
-    private readonly CarServiceContext context;
+    private readonly CarServiceContext _context;
+    public CarRepo()
+    {
+        _context = new CarServiceContext();
+    }
+
     public async Task CreateAsync(Car entity)
     {
-        using var context = new CarServiceContext();
-        context.Cars.Add(entity);
-        await context.SaveChangesAsync();
+        _context.Cars.AddAsync(entity);
+        await _context.SaveChangesAsync();
     }
 
     public async Task DeleteAsync(Guid id)
@@ -23,45 +27,28 @@ public class CarRepo : IEntityRepo<Car>
         using var context = new CarServiceContext();
         var foundCar = context.Cars.SingleOrDefault(car => car.Id == id);
         if (foundCar is null)
-            return;
+            throw new KeyNotFoundException($"Given id '{id}' was not found in database");
         context.Cars.Remove(foundCar);
         await context.SaveChangesAsync();
     }
-
-    public async Task AddAsync(Car entity)
-    {
-        AddLogic(entity, context);
-        await context.SaveChangesAsync();
-    }
-
     public async Task<List<Car>> GetAllAsync()
-    {
-        using var context = new CarServiceContext();
-        return context.Cars.ToList();
+    {        
+        return _context.Cars.ToList();
     }
 
     public async Task<Car? >GetByIdAsync(Guid id)
     {
-        using var context = new CarServiceContext();
-        return context.Cars.Where(car => car.Id == id).SingleOrDefault();
+        return _context.Cars.Where(car => car.Id == id).SingleOrDefault();
     }
 
     public async Task UpdateAsync(Guid id, Car entity)
     {
-        using var context = new CarServiceContext();
-        var foundCar = context.Cars.SingleOrDefault(car => car.Id == id);
+        var foundCar = _context.Cars.SingleOrDefault(car => car.Id == id);
         if (foundCar is null)
-            return;
+            throw new KeyNotFoundException($"Given id '{id}' was not found in database");
         foundCar.Brand = entity.Brand;
         foundCar.Model = entity.Model;
         foundCar.RegistrationNumber = entity.RegistrationNumber;
-        await context.SaveChangesAsync();
-    }
-    private void AddLogic(Car entity, CarServiceContext context)
-    {
-        if (entity.Id != Guid.Empty)
-            throw new ArgumentException("Given entity should not have Id set", nameof(entity));
-
-        context.Cars.Add(entity);
+        await _context.SaveChangesAsync();
     }
 }
